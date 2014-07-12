c9de78b55cf5545342a7e3fdf707838f7b276766
TravisIngram <ingramtravis@gmail.com>
2014-07-12 00:44:36 -0500

    add question route, add prop to db

--- question_box.rb
+++ question_box.rb
@@ -19,40 +19,45 @@ class Question_box
   property :question, Text
   property :likes, Integer
   property :created_at, DateTime
+  property :author, String
 end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
-get "/" do            # effectivily a GET / http/1.0
-  @question_box = Question_box.all
-  erb :home
-end
-
-get "/questions/:created_by" do
-  requested_author = params[:created_by]
-  @question_box = Question_box.get(requested_author)
-  erb :show
-end
-
-get "/questions/:created_by" do
+get "/" do                          # effectivily a GET / HTTP/1.0
+  @question_box = Question_box.all  # what will be returned as a response
+  erb :home                         # based on the home.erb
+end                                 # if home were commented out, we would send our db
+                                    # which would cause a 'no string value' error
+get "/questions/new" do
   @question_box = Question_box.new
   erb :new_question
 end
 
 post "/questions" do
   question_box_attributes = params["question_box"]
-  # We'll get the starting attributes for this wall from `params` that came in
-  # from `views/new_wall.erb`
+  # We'll get the starting attributes for this question from `params`
+  # that came in from `views/new_question.erb`
 
   question_box_attributes["created_at"] = DateTime.now
   # And add in the `created_at` attribute with the current time.
 
-  @question_box = Question_box.create(wall_attributes)
+  @question_box = Question_box.create(question_box_attributes)
   
   if @question_box.saved?
     redirect "/"
   else
-    erb :home
+    erb :new_question
   end
 end
+
+#  get "/questions/:author" do
+# #  @question_box = Question_box.get(requested_author)
+# #  requested_author = params[:created_by]
+#   @question_box = Question_box.get(params[:author])
+#   erb :show
+#  end
+
+
+
